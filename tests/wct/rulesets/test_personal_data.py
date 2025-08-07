53fdc2047cc17fa9e479d49fdfa8fdd78bcfea98
Liwen Knight-Zhang <registration@knight-zhang.com>
2025-08-07 15:04:47 +0100

    test: add dummy test to prevent pytest exit code 5 error

--- tests/wct/rulesets/test_personal_data.py
+++ tests/wct/rulesets/test_personal_data.py
@@ -1,1 +1,7 @@
 """Unit tests for PersonalDataRuleset class."""
+
+
+# Dummy test to supress pytest Error: Process completed with exit code 5.
+# TODO: Remove this test once actual tests are implemented.
+def test_dummy():
+    pass
